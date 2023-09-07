6a0b0959a32542a49cc1b6458d73473fc006d21d
sawyer315 <143819017+sawyer315@users.noreply.github.com>
2023-09-07 14:30:46 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+
+print("First number: ")
+num1 = int(input())
+print("Second number: ")
+num2 = int(input())
+print("Third number: ")
+num3 = int(input())
+print("Fourth number: ")
+num4 = int(input())
+
+sum = num1 + num2 + num3 + num4
+avg = sum / 4
+
+print("")
+print("The sum of the four numbers is: ", sum)
+print("")
+print("The average of the four numbers is: ", avg)
